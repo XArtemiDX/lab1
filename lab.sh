65f76c6157da43987304806f58af2cf6049c04cf
XArtemiDX <94452887+XArtemiDX@users.noreply.github.com>
2024-05-21 13:48:14 +0800

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -8,7 +8,7 @@ touch edited.txt
 
 for file in "$directory"/*.txt; do
     if grep -q "$old_word" "$file"; then
-        sed -i "s/$old_word/$new_word/g" "$file"
+        sed -i "s/$old_word/$new_word/" "$file"
         echo "$file" >> edited.txt
     fi
 done
