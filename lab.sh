c1d8725e071f6779386d591af49d3718ee8c1fe1
XArtemiDX <94452887+XArtemiDX@users.noreply.github.com>
2024-05-21 13:53:37 +0800

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -2,15 +2,15 @@ echo "Лаб 3"
 #!/bin/bash
 
 read -p "Путь к директории с txt файлами: " directory
-read -p "Введите слово, которое нужно заменить в файлах '$directory': " old_word
-read -p "Введите слово для замены '$old_word': " new_word
+read -p "Введите слово, которое нужно заменить в файлах '$directory': " old
+read -p "Введите слово для замены '$old': " new
 touch edited.txt
 
-for file in "$directory"/*.txt; do
-    if grep -q "$old_word" "$file"; then
-        sed -i "s/$old_word/$new_word/" "$file"
-        echo "$file" >> edited.txt
+for file in $(find . -type f -name "*.txt"); do
+    if grep -q "$old" "$file"; then
+        sed -i "s/$old/$new/" "$file"
+        echo "Замена '$old' на '$new' в файле '$file'" >> edited.txt
     fi
 done
 
-echo "Замена слова '$old_word' на '$new_word' завершена. Измененные файлы отображены в edited.txt"
+echo "Замена слова '$old' на '$new' завершена. Измененные файлы отображены в edited.txt"
